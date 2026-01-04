464eb1a878e2ee8e9fa8e1921cec3f6a459f5d06
TheurgicThaps <thapelotlowana58@gmail.com>
2026-01-04 18:56:07 +0200

    fixed description

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*============typing animation ================= */
 var typed = new Typed(".typing", {
-  strings: ["Web Designer", "Web Developer", "Graphic Designer", "Witsie", "Knockandian", "Mobile App Developer","Pornstar"],
+  strings: ["Web Designer", "Web Developer", "Graphic Designer", "Witsie", "Knockandian", "Mobile App Developer"],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true,
